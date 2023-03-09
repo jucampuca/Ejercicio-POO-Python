46a0138677aeacbecf7226b5a4a69e3538079486
Juan Cardona <jucampuca@gmail.com>
2023-03-09 15:36:35 -0500

    Iniciamos el tema de funciones lambda

---
+++ FUNCIONES_LAMBDA/01-FuncionesLambda.py
@@ -0,0 +1,25 @@
+# FUNCIONES LAMBDA
+
+#! Las funciones lambda son funciones anónimas:
+    #? Permiten múltiples argumentos.
+    #? Solo puede tener una expresión.
+    #? Estructura: lambda arg1, arg2 : expression
+
+#! Apartado 1: Definir una función lambda.
+
+
+
+
+#! Apaertado 2: Llamar funciones dentro de una lambda.
+#? Podemos llamar otras funciones dentro de la expresion de la función lambda.
+
+
+
+
+#! Apartado 3: Funciones lambda dentro de funciones.
+#? Podemos definir funciones lambda dentro de funciones convencionales.
+#** Gracias a esto podemos definir funciones lambda con distintos parámetros.
+
+
+
+
